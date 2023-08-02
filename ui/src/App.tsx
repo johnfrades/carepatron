c3d05a26d93d483a24c48f670f68af7ab8f8e1e3
JohnF <johnfrades@gmail.com>
2023-08-02 21:19:57 +0800

    feat: add css baseline for global reset

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,22 +1,23 @@
-import React from "react";
-import {Route, Routes} from "react-router-dom";
-import "./App.css";
-import DataProvider from "./store/DataProvider";
-import Clients from "./pages/Clients";
-import { ThemeProvider } from '@mui/material';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import DataProvider from './store/DataProvider';
+import Clients from './pages/Clients';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './theme';
 
 export default function App() {
-  return (
-    <div className="App">
-        <ThemeProvider theme={theme}>
-          <DataProvider>
-            <Routes>
-              <Route path="/" element={<Clients />} />
-              <Route path="/Clients" element={<Clients />} />
-            </Routes>
-          </DataProvider>
-        </ThemeProvider>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<CssBaseline />
+			<ThemeProvider theme={theme}>
+				<DataProvider>
+					<Routes>
+						<Route path='/' element={<Clients />} />
+						<Route path='/Clients' element={<Clients />} />
+					</Routes>
+				</DataProvider>
+			</ThemeProvider>
+		</div>
+	);
 }
